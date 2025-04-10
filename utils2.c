34bc0654fb553139c30355442feb32f4a7853685
Abdelkebir Dahmani <abdahman@c4r7p7.1337.ma>
2025-04-10 22:00:52 +0100

    puush

--- utils2.c
+++ utils2.c
@@ -6,7 +6,7 @@
 /*   By: abdahman <abdahman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 13:09:32 by abdahman          #+#    #+#             */
-/*   Updated: 2025/04/10 21:43:41 by abdahman         ###   ########.fr       */
+/*   Updated: 2025/04/10 21:57:21 by abdahman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ void	put_map_to_win(t_game *g)
 		j = 0;
 		while (g->map[i][j] && g->map[i][j] != '\n')
 		{
-			printf ("%c, ", g->map[i][j]);
 			if (g->map[i][j] == '1')
 				put_img(g, g->a1_img, j * g->w_s, i * g->w_s);
 			else if (g->map[i][j] == '0')
